62063c0a38ec3ff7df75d722c657a5539e7851b3
Bachir Soussi Chiadmi <bachir@g-u-i.net>
2014-01-17 12:00:27 +0100

    ajax call, php load google drive csv, store it as serialzed data into data.txt, return json

--- ajax.php
+++ ajax.php
@@ -1,12 +1,61 @@
 <?php 
 
-# load csv
+$ret = array(
+  "test" => "hello!"
+);
 
-# parse csv
+$drive_csv_url = "https://docs.google.com/spreadsheet/pub?key=0AouWOA7wSzR-dG14ZUFuc2t5RlR4dk1DdjZKZUJtR1E&single=true&gid=0&output=csv";
+
+if(!ini_set('default_socket_timeout',    15)) 
+  $ret['errors'][] = "unable to change socket timeout";
+
+$data = array();
+
+if (false && ($fh = fopen($drive_csv_url, "r")) !== FALSE){
+  # if we can connect to drive spreadsheet
+  while ( ($row = fgetcsv($fh, 5000, ",")) !== FALSE) {
+      $data[] = $row;
+  }
+  fclose($fh);
+  
+  # we record it as a local serialized array backup in case of we wont have internet connection any more
+  $data_str = serialize($data);
+  $fh_txt = fopen('assets/data/data.txt', 'r+');
+  $fwrite = fwrite($fh_txt, $data_str);
+  if($fwrite){
+    $ret['messages'][] = 'data stored in data.txt from google drive';
+  }else{
+    $ret['errors'][] = 'problem occure while stored data from google drive to data.txt';
+  }
+  fclose($fh_txt);
+  
+}else{
+  $ret['errors'][] = "Problem reading google drive csv, you may check your internet connection";
+  # load data from local serialized array
+  $data = unserialize(file_get_contents("assets/data/data.txt"));
+}
+
+$ret['data'] = $data;
 
 # check time
 
+
+
+
+
 # return json
+returnJson($ret);
 
 
- ?>
+/**
+* Helpers
+*/
+
+
+
+function returnJson($array){
+  header('Content-Type: application/json');
+  echo json_encode($array);
+}
+
+?>
--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <html>
 <head>
   <title>Encyclopedie de la parole - Parc Saint Léger</title>
-  <link rel="stylesheet" type="text/css" href="stryles.css">
-  <script type="text/javascript" src="script.js"></script>
-
+  <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+  <script type="text/javascript" src="assets/js/jquery-1.10.2.js"></script>
+  <script type="text/javascript" src="assets/js/script.js"></script>
 </head>
 <body>
-
+  <div id="root">
+    <h1>Encyclopedie de la parole</h1>
+  </div>
 </body>
 </html>
